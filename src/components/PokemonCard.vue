72d6fc54da2d341fd7d33f572c3f4f587ae02cb9
Matheus Penna <matheus.penna@blitzpay.com.br>
2024-04-13 16:56:37 -0300

    create pokemon types

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -4,7 +4,7 @@
       <NuxtImg v-if="info" class="poke-img" :src="info.sprites.front_default" :alt="data.name" />
       <span class="font-bold mb-2">{{ data.name }}</span>
       <span v-if="info" class="text-sm font-bold text-[9px] mb-8">Cód {{ info.id }}</span>
-      <div class="flex items-center justify-center flex-wrap">
+      <div v-if="info" class="flex items-center justify-center flex-wrap">
         <template v-for="(item, i) in info.types">
           <span 
             class="type-tag flex items-center justify-center text-white py-1 px-2 uppercase text-[8px]" 
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,16 +1,93 @@
-interface PokemonType {
-  name: String;
+export interface PokemonType {
+  name: String | string | undefined;
   url: String | string;
 }
 
-interface PokemonInfoType {
-  name: String;
-  url: String | string;
-  id: string | number;
+export interface NamedAPIResource extends PokemonType {}
+
+export interface PokemonKindType {
+  slot: number;
+  type: NamedAPIResource;
+}
+
+export interface PokemonAbility {
+  is_hidden: boolean;
+  slot: number;
+  ability: NamedAPIResource;
+}
+
+export interface PokemonInfoType {
+   id: number;
+   name: string;
+   base_experience: number;
+   height: number;
+   is_default: boolean;
+   order: number;
+   weight: number;
+   abilities: PokemonAbility[];
+   forms: NamedAPIResource[];
+   game_indices: VersionGameIndex[];
+   held_items: PokemonHeldItem[];
+   location_area_encounters: string;
+   moves: PokemonMove[];
+   sprites: PokemonSprites;
+   species: NamedAPIResource;
+   stats: PokemonStat[];
+   types: PokemonKindType[];
+}
+
+export interface PokemonHeldItem {
+  item: NamedAPIResource;
+  version_details: PokemonHeldItemVersion[];
+}
+
+export interface PokemonHeldItemVersion {
+  version: NamedAPIResource;
+  rarity: number;
+}
+
+export interface PokemonMove {
+  move: NamedAPIResource;
+  version_group_details: PokemonMoveVersion[];
+}
+
+export interface PokemonMoveVersion {
+  move_learn_method: NamedAPIResource;
+  version_group: NamedAPIResource;
+  level_learned_at: number;
+}
+
+export interface PokemonSprites {
+  front_default: string;
+  front_shiny: string;
+  front_female: string;
+  front_shiny_female: string;
+  back_default: string;
+  back_shiny: string;
+  back_female: string;
+  back_shiny_female: string;
+  other?: {
+      [key in string]?: {
+          [spriteName in string]?: string | null;
+      };
+  };
+  versions?: {
+      [version in string]?: {
+          [gameTitle in string]?: {
+              [spriteName in string]?: string;
+          };
+      };
+  };
+}
+
+export interface PokemonStat {
+  stat: NamedAPIResource;
+  effort: number;
+  base_stat: number;
 }
 
 
-export type {
-  PokemonType,
-  PokemonInfoType
+export interface VersionGameIndex {
+  game_index: number;
+  version: NamedAPIResource;
 }
